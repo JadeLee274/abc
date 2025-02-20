7a48d300c99db116bff75b68759af1e0d5df4502
JadeLee274 <tmdals274@snu.ac.kr>
2025-02-20 12:02:09 +0900

    update arithmetic

--- arithmetic.py
+++ arithmetic.py
@@ -1,1 +1,12 @@
-print("Hello World, do you like minecraft?")
+def add(a: float, b:float) -> float:
+    return a + b
+
+def sub(a: float, b: float) -> float:
+    return a - b
+
+def mult(a: float, b: float) -> float:
+    return a * b
+
+def div(a: float, b: float) -> float:
+    assert b != 0, "division error"
+    return a / b
