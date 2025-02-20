90ab22a6dc8ed9f4ec2a87d018b6e56cd3d06838
JadeLee274 <tmdals274@snu.ac.kr>
2025-02-20 11:43:23 +0900

    edit prime

--- prime.py
+++ prime.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 def is_prime(n: int) -> bool:
-    for i in range(1, np.sqrt(n)+1):
-        if n % i == 0:
-            return False
-        else:
-            return True
+    if n == 1:
+        return False
+    else:
+        for i in range(1, np.sqrt(n) + 1):
+            if n % i == 0:
+                return False
+            else:
+                return True
